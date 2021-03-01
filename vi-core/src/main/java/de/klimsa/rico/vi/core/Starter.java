b73f92d6d0e238181c78475889b06fb6d6e3d4ee
ricok <ricok@192.168.0.5>
2021-03-01 22:12:33 +0100

    Initial commit

---
+++ vi-core/src/main/java/de/klimsa/rico/vi/core/Starter.java
@@ -0,0 +1,16 @@
+package de.klimsa.rico.vi.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starter {
+		
+	private static final Logger LOG = LoggerFactory.getLogger(Starter.class);
+	
+	public static void main(String[] args) {
+		LOG.info("System started");
+		
+		LOG.info("System stopped");
+	}
+
+}
