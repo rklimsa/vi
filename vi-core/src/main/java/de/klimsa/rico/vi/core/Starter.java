ed7574d9f63d8a74943baea03f4658606575ad70
ricok <ricok@192.168.0.5>
2021-03-28 15:20:28 +0200

    Some testing

--- vi-core/src/main/java/de/klimsa/rico/vi/core/Starter.java
+++ vi-core/src/main/java/de/klimsa/rico/vi/core/Starter.java
@@ -1,5 +1,9 @@
 package de.klimsa.rico.vi.core;
 
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +13,14 @@ public class Starter {
 	
 	public static void main(String[] args) {
 		LOG.info("System started");
+
+		OntModelSpec ontModelSpec = new OntModelSpec(OntModelSpec.OWL_DL_MEM_RULE_INF);
+		
+		OntModel ontModel = ModelFactory.createOntologyModel(ontModelSpec);
+		
+		Model model = ontModel.begin();
+		
+		ontModel.commit();
 		
 		LOG.info("System stopped");
 	}
